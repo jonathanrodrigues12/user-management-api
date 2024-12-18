3daae4199a221bc7c98d160490c0a6bfa2edf0cb
Jonathan Rodrigues <jonathanrodrigues12@gmail.com>
2024-12-18 16:44:25 -0300

    enable cors

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,11 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    allowedHeaders: 'Content-Type, Accept, Authorization',
+  });
 
   const config = new DocumentBuilder()
     .setTitle('User Management API')
